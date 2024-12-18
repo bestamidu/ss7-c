a5d72c1905ed603c4fc365c941ee16916cdab6e5
bestamidu <ynhu22887@gmail.com>
2024-12-18 23:25:09 +0700

    2

---
+++ bt2 ss7.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a [5];
+	for (int i=0;i<5 ; i++){
+		
+	printf ("nhap phan tu thu %d =",i);
+	
+	scanf("%d",&a[i]);}
+	
+	for (int i=0;i<5 ; i++){
+		
+	printf ("phan tu thu %d = %d\n",i,a[i]);}
+	
+	return 0;
+	
+	}
