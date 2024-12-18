2ef5af4a64e0434e34d97f8dec5a5f5b4d80fc11
bestamidu <ynhu22887@gmail.com>
2024-12-18 23:24:52 +0700

    1

---
+++ bt1 ss7.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int n[5]={2,4,6,7,9};
+	
+	int sizen= sizeof(n)/sizeof(int);
+	printf("do dai cua mang la %d",sizen);
+	for(int i=0 ; i<5 ; i++){
+		printf(" gia tri %d la %d\n ",i,n[i]);
+	}
+	return 0;
+}
